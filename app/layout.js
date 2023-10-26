07fe933cae65880343819c340bd5b403257b2b6f
Owen Rhodes <owex@users.noreply.github.com>
2023-10-26 14:33:45 +0100

    Fix: Seo for home and GTM

--- app/layout.js
+++ app/layout.js
@@ -61,7 +61,7 @@ const Layout = ({ children }) => {
           <meta name="robots" content="noindex,nofollow" />
         )}
 
-        {env === 'development' && (
+        {env !== 'development' && (
           <Script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
--- app/page.js
+++ app/page.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 import HomePage from './HomePage';
 
-const Page = props => {
-  return <HomePage />
+
+
+export const metadata = {
+  title: 'Devools',
+  description:
+    'Providing code snippets and tools to help you speed up your development time.',
+  openGraph: {
+    title: 'Home Page ',
+    description:
+      'Providing code snippets and tools to help you speed up your development time.',
+  },
+};
+
+const Page = (props) => {
+  return <HomePage />;
 };
 
 export default Page;

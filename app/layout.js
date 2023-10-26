e746f26ddbd44e2853ce9b90684a700890b26d26
Owen Rhodes <owex@users.noreply.github.com>
2023-10-26 11:15:24 +0100

    Fix: style tag

--- app/layout.js
+++ app/layout.js
@@ -76,7 +76,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
               height="0"
               width="0"
-              style="display:none;visibility:hidden"
+              style={{ display: 'none', visibility: 'hidden' }}
             ></iframe>
           </noscript>
         )}
